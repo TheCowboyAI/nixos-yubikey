3b9a3b691934715e86ae7904d776db38b0708feb
Steele Price <steele@thecowboy.ai>
2024-11-02 12:34:57 -0700

    add motd

--- x86/configuration.nix
+++ x86/configuration.nix
@@ -9,6 +9,7 @@
       "${modulesPath}/profiles/hardened.nix"
       ./hardware-configuration.nix
       ./disko.nix
+      ./scripts
     ];
 
   networking.hostName = "nixos-yubikey";
@@ -51,42 +52,13 @@
     openssh
     jq
     jc
+    glow
 
     yubikey-manager
     yubikey-manager-qt
     yubikey-personalization
     age-plugin-yubikey
     piv-agent
-
-    #scripts
-    (import scripts/completely-reset-my-yubikey.nix { inherit pkgs; })
-    (import scripts/edit-env.nix { inherit pkgs; })
-    (import scripts/enable-fido2.nix { inherit pkgs; })
-    (import scripts/enable-pgp-touch.nix { inherit pkgs; })
-    (import scripts/enable-piv-touch.nix { inherit pkgs; })
-    (import scripts/get-env.nix { inherit pkgs; })
-    (import scripts/get-serials.nix { inherit pkgs; })
-    (import scripts/get-status.nix { inherit pkgs; })
-    (import scripts/make-certkey.nix { inherit pkgs; })
-    (import scripts/make-domain-cert.nix { inherit pkgs; })
-    (import scripts/make-rootca.nix { inherit pkgs; })
-    (import scripts/make-subkeys.nix { inherit pkgs; })
-    (import scripts/make-tls-client.nix { inherit pkgs; })
-    (import scripts/random-6.nix { inherit pkgs; })
-    (import scripts/random-8.nix { inherit pkgs; })
-    (import scripts/random-mgmt-key.nix { inherit pkgs; })
-    (import scripts/random-pass.nix { inherit pkgs; })
-    (import scripts/set-attributes.nix { inherit pkgs; })
-    (import scripts/set-env.nix { inherit pkgs; })
-    (import scripts/set-fido-pin.nix { inherit pkgs; })
-    (import scripts/set-fido-retries.nix { inherit pkgs; })
-    (import scripts/set-oauth-password.nix { inherit pkgs; })
-    (import scripts/set-pgp-pins.nix { inherit pkgs; })
-    (import scripts/set-piv-pins.nix { inherit pkgs; })
-    (import scripts/set-yubikey.nix { inherit pkgs; })
-    (import scripts/verify-xfer.nix { inherit pkgs; })
-    (import scripts/xfer-certs.nix { inherit pkgs; })
-    (import scripts/xfer-keys.nix { inherit pkgs; })
   ];
 
   services.udev.packages = with pkgs; [
@@ -144,4 +116,13 @@
   };
 
   services.getty.autologinUser = lib.mkForce "yubikey";
+
+  environment.etc."doc/help.md".source = ./scripts/readme.md;
+
+  users.motd = ''
+    NixOS Yubikey Configuration
+    ==========================
+
+    For instructions type `help`
+  '';
 }
